3a695cb0a316dfd5ee43421090528bb08ae9aae4
danny@gimenez.biz <danny@gimenez.biz>
2023-08-01 19:53:16 -0400

    Fixed bad public-api, and api to docs

--- projects/signal-generators/src/public-api.ts
+++ projects/signal-generators/src/public-api.ts
@@ -1,8 +1,9 @@
 /*
  * Public API Surface of signal-generators
  */
-
-export { SignalInput, isToSignalInput } from './lib/signal-input';
+export { SignalInput } from './lib/signal-input';
 export { ValueSource }  from './lib/value-source';
 export * from './lib/generators/debounce-signal';
+export * from './lib/generators/map-signal';
+export * from './lib/generators/sequence-signal';
 export * from './lib/generators/timer-signal';
