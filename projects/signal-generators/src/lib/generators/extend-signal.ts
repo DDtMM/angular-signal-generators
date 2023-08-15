de97a0c8d28388824af47a8811777034cecf25c5
danny@gimenez.biz <danny@gimenez.biz>
2023-08-15 08:48:13 -0400

    extend signal "progress"

--- projects/signal-generators/src/lib/generators/extend-signal.ts
+++ projects/signal-generators/src/lib/generators/extend-signal.ts
@@ -1,17 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Signal, WritableSignal, isSignal, signal } from '@angular/core';
 
 export type WritableSignalProxy<T> = Pick<WritableSignal<T>, 'mutate' | 'set' | 'update'>;
 
-export type ProxyMethod<T, F extends (signalProxy: WritableSignalProxy<T>, ...args: readonly any[]) => unknown> =
+
+export type ProxyMethodBase<T> = (signalProxy: WritableSignalProxy<T>, ...args: readonly any[]) => any;
+
+export type ProxyMethodExplicitArgs<T, A extends readonly any[]> = (signalProxy: WritableSignalProxy<T>, ...args: A) => any;
+
+export type ProxyMethodAlt<T, F extends (signalProxy: WritableSignalProxy<T>, ...args: readonly any[]) => any> =
+  F extends (...args: infer P) => infer R
+    ? (args: P) => R
+    : never;
+
+
+export type ProxyMethod<T, F extends ProxyMethodBase<T>> =
   F extends (signalProxy: WritableSignalProxy<T>, ...args: infer P) => infer R
     ? (signalProxy: WritableSignalProxy<T>, ...args: P) => R
     : never;
 
-export type ProxyMethodParams<F extends ProxyMethod<unknown, () => any>> =
+
+
+export type ProxyMethodParams<F extends ProxyMethodAlt<any, () => any>> =
   F extends (signalProxy: WritableSignalProxy<any>, ...args: infer P) => any ? P : never;
 
-export type Methods<T, K extends string | number | symbol> = { [Key in K]: ProxyMethod<T, () => void> };
+export type Methods<T, K extends string> = {
+  [Key in K]: ProxyMethod<T, (...args: unknown[]) => unknown>
+};
+export type Methods2<T, K extends string> = {
+  [Key in K]: ProxyMethodExplicitArgs<T, any>
+};
+export type ProxyMethods<T, M extends Methods<T, string>> = {
+  [Key in keyof M]: M[Key] extends (signalProxy: WritableSignalProxy<T>, ...args: infer P) => infer R
+    ? (signalProxy: WritableSignalProxy<T>, ...args: P) => R
+    : never;
+};
+const attempt1: Methods<number, 'help'> = { help: (x: WritableSignalProxy<number>, y: number) => x.set(y) };
+const attempt2: Methods2<number, 'help'> = { help: (x: WritableSignalProxy<number>, y: number) => x.set(y) };
+const attempt3: ProxyMethods<number, Methods<number, 'help'>> = { help: (x: WritableSignalProxy<number>, y: number) => x.set(y) }
+
 
 export type OutputMethod<F extends ProxyMethod<any, () => any>> =
   F extends ProxyMethod<infer T, infer F2>
@@ -21,9 +49,9 @@ export type OutputMethod<F extends ProxyMethod<any, () => any>> =
 export type OutputMethodParams<F extends ProxyMethod<any, () => any>> =
     F extends (signalProxy: WritableSignalProxy<any>, ...args: infer P) => any ? P : never;
 
-export type OutputMethods<M extends Methods<any, string | number | symbol>> = { [Key in keyof M]: OutputMethod<M[Key]> };
+export type OutputMethods<M extends Methods<any, string>> = { [Key in keyof M]: OutputMethod<M[Key]> };
 
-export function extendSignal<T, const M extends Methods<T, string | number | symbol>>(
+export function extendSignal<T, const M extends Record<string, ProxyMethod<T, any>>>(
   valueSource: Exclude<T, Signal<unknown>> | WritableSignal<T>,
   methods: M
   ):
@@ -53,6 +81,10 @@ export function extendSignal<T, const M extends Methods<T, string | number | sym
   return Object.assign(output, assignMethods as OutputMethods<M>);
 }
 
-const yyy = extendSignal(1, { doSomething: (proxy, a: number) => proxy.set(a + 5)});
+const yyy = extendSignal(1, {
+  doSomething: (proxy, a: number) => proxy.set(a + 5),
+  doSomethingElse: (proxy, x: string, y: string) => proxy.update(z => z + x.length + y.length)
+});
 
 yyy.doSomething(5);
+
