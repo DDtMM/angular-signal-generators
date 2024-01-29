572cd52bf6291b1df1c499443ab6105ef7c996c1
danny@gimenez.biz <danny@gimenez.biz>
2024-01-28 21:23:21 -0500

    Merged 1.4.2 changes for testing.  Successful

--- projects/signal-generators/src/testing/signal-testing-utilities.ts
+++ projects/signal-generators/src/testing/signal-testing-utilities.ts
@@ -1,13 +1,29 @@
 import { Signal, WritableSignal, isSignal, signal } from '@angular/core';
-import { ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, tick } from '@angular/core/testing';
 import { isMethodKey } from '../lib/internal/utilities';
 
+interface AutoDetectChangesSignalOptions {
+  /** If in fakeAsync context call tick in addition to detectChanged */
+  tickAfter?: boolean;
+}
 /** Adds fixture.detectChanges after every method call of a signal. */
-export function autoDetectChangesSignal<T, S extends Signal<T>>(fixture: ComponentFixture<unknown>, source: S): S
+export function autoDetectChangesSignal<T, S extends Signal<T>>(
+  fixture: ComponentFixture<unknown>,
+  source: S,
+  options?: AutoDetectChangesSignalOptions
+): S;
 /** Creates a writable signal that has fixture.detectChanges run after every method call. */
-export function autoDetectChangesSignal<T>(fixture: ComponentFixture<unknown>, source: T): WritableSignal<T>
-export function autoDetectChangesSignal<T, S extends Signal<T>>(fixture: ComponentFixture<unknown>, source: S | T): S | WritableSignal<T> {
-
+export function autoDetectChangesSignal<T>(
+  fixture: ComponentFixture<unknown>,
+  source: T,
+  options?: AutoDetectChangesSignalOptions
+): WritableSignal<T>;
+export function autoDetectChangesSignal<T, S extends Signal<T>>(
+  fixture: ComponentFixture<unknown>,
+  source: S | T,
+  options?: AutoDetectChangesSignalOptions
+): S | WritableSignal<T> {
+  const detectChanges = createDetectChangesFn();
   const output = isSignal(source) ? source : signal(source);
 
   for (const key in output) {
@@ -17,15 +33,26 @@ export function autoDetectChangesSignal<T, S extends Signal<T>>(fixture: Compone
       });
     }
   }
-
+  detectChanges();
   return output;
 
   /** This will wrap autoDetectChanges after the function call. */
-  function addDetectChangesToFunction <TArgs extends unknown[], TOut>(fn: (...args: TArgs) => TOut) {
+  function addDetectChangesToFunction<TArgs extends unknown[], TOut>(fn: (...args: TArgs) => TOut) {
     return (...args: TArgs) => {
       const res = fn.apply(output, args);
-      fixture.detectChanges();
+      detectChanges();
       return res;
+    };
+  }
+
+  /** Creates a function to call after changes. */
+  function createDetectChangesFn(): () => void {
+    if (options?.tickAfter) {
+      return () => {
+        fixture.detectChanges();
+        tick();
+      };
     }
+    return () => fixture.detectChanges();
   }
 }
