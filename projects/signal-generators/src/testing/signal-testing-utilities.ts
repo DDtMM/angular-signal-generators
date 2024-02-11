d5502b0ede01b88c28ff91a7c9b01a04c63ab0e5
danny@gimenez.biz <danny@gimenez.biz>
2024-02-11 09:49:26 -0500

    Removed old autoDetectChangesSignal

--- projects/signal-generators/src/testing/signal-testing-utilities.ts
+++ projects/signal-generators/src/testing/signal-testing-utilities.ts
@@ -1,6 +1,5 @@
 import { Signal, WritableSignal, isSignal, signal } from '@angular/core';
 import { ComponentFixture } from '@angular/core/testing';
-import { isMethodKey } from '../lib/internal/utilities';
 
 /** Wraps a signal with a Proxy that calls change detection after each method call. */
 export function autoDetectChangesSignal<T, S extends Signal<T>>(fixture: ComponentFixture<unknown>, source: S): S;
@@ -30,30 +29,3 @@ export function autoDetectChangesSignal<T, S extends Signal<T>>(
   return proxy;
 }
 
-export function autoDetectChangesSignal2<T, S extends Signal<T>>(fixture: ComponentFixture<unknown>, source: S): S;
-/** Creates a writable signal that has fixture.detectChanges run after every method call. */
-export function autoDetectChangesSignal2<T>(fixture: ComponentFixture<unknown>, source: T): WritableSignal<T>;
-export function autoDetectChangesSignal2<T, S extends Signal<T>>(
-  fixture: ComponentFixture<unknown>,
-  source: S | T
-): S | WritableSignal<T> {
-  const output = isSignal(source) ? source : signal(source);
-
-  for (const key in output) {
-    if (isMethodKey(output, key)) {
-      Object.assign(output, {
-        [key]: addDetectChangesToFunction(output[key] as () => void)
-      });
-    }
-  }
-  return output;
-
-  /** This will wrap autoDetectChanges after the function call. */
-  function addDetectChangesToFunction<TArgs extends unknown[], TOut>(fn: (...args: TArgs) => TOut) {
-    return (...args: TArgs) => {
-      const res = fn.apply(output, args);
-      fixture.detectChanges();
-      return res;
-    };
-  }
-}
