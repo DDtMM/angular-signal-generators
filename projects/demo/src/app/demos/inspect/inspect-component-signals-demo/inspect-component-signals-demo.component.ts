32b06404791a7052a1cd1407327264a39c6418b4
danny@gimenez.biz <danny@gimenez.biz>
2024-09-27 13:36:25 -0400

    Add another inspect demo

---
+++ projects/demo/src/app/demos/inspect/inspect-component-signals-demo/inspect-component-signals-demo.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { afterNextRender, ChangeDetectionStrategy, Component, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { inspect } from 'projects/signal-generators/src/public-api';
+
+@Component({
+  selector: 'app-inspect-component-signals-demo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './inspect-component-signals-demo.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InspectComponentSignalsDemoComponent {
+  /** This is a very contrived demonstration using nested signals. */
+  readonly $luckyNumbers = signal<WritableSignal<number>[]>([]);
+  readonly $name = signal('Danny');
+
+  constructor() {
+    /** This is the easiest way to use this! */
+    inspect(this);
+    afterNextRender(() => Array(3).fill(0).forEach(() => this.addLuckyNumber()));
+  }
+  addLuckyNumber() {
+    this.$luckyNumbers.update(numbers => [...numbers, signal(Math.floor(Math.random() * 49) + 1)]);
+  }
+  reroll() {
+    this.$luckyNumbers().forEach(x => x.set(Math.floor(Math.random() * 49) + 1));
+  }
+}
