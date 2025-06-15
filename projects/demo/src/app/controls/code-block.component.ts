e16654626a49bf469a9d527d7467154b5e3b4f8c
Daniel Gimenez <4720650+DDtMM@users.noreply.github.com>
2025-06-15 12:21:35 -0400

    Add selector as an option to timerSignal.

--- projects/demo/src/app/controls/code-block.component.ts
+++ projects/demo/src/app/controls/code-block.component.ts
@@ -5,7 +5,7 @@ import { CopyButtonComponent } from './copy-button.component';
 /** Renders a demo with sources.  The demo component should be passed as content. */
 @Component({
   selector: 'app-code-block',
-  host: { class: 'block relative bg-slate-50 dark:bg-slate-800 whitespace-pre-wrap w-full overflow-auto' },
+  host: { class: 'block relative  whitespace-pre-wrap w-full overflow-auto' },
   imports: [CopyButtonComponent, HighlightModule],
   template: `
   @if($showCopy()) {
@@ -15,7 +15,7 @@ import { CopyButtonComponent } from './copy-button.component';
       </div>
     </div>
   }
-  <code class="h-full w-full whitespace-pre bg-slate-50 dark:bg-slate-800" [highlight]="$content()" [language]="$language()"></code>
+  <code class="h-full min-w-full w-fit whitespace-pre bg-slate-50 dark:bg-slate-800 " [highlight]="$content()" [language]="$language()"></code>
   <!-- this is here only to copy from transcluded content -->
   <div #contentWrapper class="hidden" aria-hidden="true"><ng-content /></div>
   `,
